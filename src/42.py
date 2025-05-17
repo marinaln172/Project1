6b8fda9eea397e009a1bddd179158ce45b3fd30b
marinaln172 <marinaln172@gmail.com>
2025-05-17 05:50:50 +0000

    Add 42.py

---
+++ src/42.py
@@ -0,0 +1,8 @@
+import math
+
+def calculate_square_root(number):
+    return math.sqrt(number)
+
+number = 9.0
+square_root = calculate_square_root(number)
+print(f"The square root of {number} is: {square_root}")
